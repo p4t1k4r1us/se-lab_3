ce731c5516ccfc9dc65f5697c87d6b71a23e72d9
p4t1k4r1us <szbakosb@gmail.com>
2025-10-18 16:10:52 +0200

    unused value removed

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -33,7 +33,7 @@ public class TorpedoStore {
       new IllegalArgumentException("numberOfTorpedos");
     }
 
-    boolean success = false;
+    boolean success;
 
     // simulate random overheating of the launcher bay which prevents firing
     Random generator = new Random();
